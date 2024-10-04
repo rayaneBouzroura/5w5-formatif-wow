43004a6209c477c3489d3b78c7794f35aea4946d
rayaneBouzroura <2111752@cegepmontpetit.ca>
2024-10-03 20:14:17 -0400

    finished exo reactiveForm

--- Intra_Formatif_H24-main/question3/src/app/app.component.ts
+++ Intra_Formatif_H24-main/question3/src/app/app.component.ts
@@ -1,19 +1,82 @@
 import { Component } from '@angular/core';
-import { AbstractControl, FormBuilder, ValidationErrors, Validators } from '@angular/forms';
+import { AbstractControl, FormBuilder, FormGroup, ValidationErrors, Validators } from '@angular/forms';
+interface FormData {
+  name? : string | null;
+  roadnumber? : string | null;
+  rue? : string | null;
+  postalcode? : string | null;
+  comments? : string | null;
 
+}
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
+  form:FormGroup<any>;
+  formData : FormData = {};
+
+  constructor(fb : FormBuilder){
+    this.form = fb.group({
+      name : ['',[Validators.required]],
+      roadnumber : ['',[Validators.required,Validators.min(1000),Validators.max(9999)]],
+      rue : ['',[Validators.required]],
+      postalcode : ['',[Validators.pattern("^[A-Z][0-9][A-Z][ ]?[0-9][A-Z][0-9]$")]],
+      comments : ['',[this.myValidator]],
+
+    }, {validators : this.nameInCommentValidator},);
+
+    this.form.valueChanges.subscribe(()=>{
+      this.formData = this.form.value
+      console.log(this.form.value);
+    });
+  }
   title = 'reactive.form';
 
-  
+  myValidator(control : AbstractControl) : ValidationErrors | null {
 
-  constructor(
-    
-  ) { }
+    const comment = control.value;
+    console.log(comment);
+    if(!comment){
+      return null;
+    }
+    const count : number= comment.split(' ').length;
+    console.log('num of words : ' , count);
+
+    if(count < 10){
+      return {notEnoughWords : true};
+    }
+    else{
+      return null;
+    }
+
+
+  }
+  nameInCommentValidator (control : AbstractControl) : ValidationErrors | null {
+    const comment : string |null = control.get('comments')?.value;
+    const name :string |null= control.get('name')?.value;
+    if(!comment || !name ){
+      return null;
+    }
+
+    console.log('current comment ',comment);
+    console.log('current name ',name);
+    let bool : boolean = comment.includes(name);
+    if(bool){
+      return {containsName : true}
+    }else
+    {
+      return null
+    }
+
+
+  }
+
+
+
+
+
 }
 
 
